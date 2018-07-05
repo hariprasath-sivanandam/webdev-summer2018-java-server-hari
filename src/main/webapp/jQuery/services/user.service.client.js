1e14f40ad13535c4105738449b94dcdf1379b9e7
hariprasath <sivanandam.h@husky.neu.edu>
2018-07-05 12:52:51 -0400

    modified fetch call url

--- src/main/webapp/jQuery/services/user.service.client.js
+++ src/main/webapp/jQuery/services/user.service.client.js
@@ -3,7 +3,7 @@ function UserServiceClient(){
     var self=this;
 
     function findAllUsers(){
-        return fetch("http://localhost:8080/api/user")
+        return fetch("/api/user")
             .then(function (response){
                 return response.json();
         })
